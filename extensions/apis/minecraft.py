1b84e4e309d6347962096c6a5a724fcdf27fa727
Fam0r <fam0r@mailbox.org>
2022-05-13 16:04:38 +0300

    Add minecraft server info command

--- extensions/apis/minecraft.py
+++ extensions/apis/minecraft.py
@@ -5,6 +5,11 @@ import utils
 import json
 import base64
 from datetime import datetime
+import socket
+import time
+import struct
+from typing import Tuple
+from io import BytesIO
 
 
 class MinecraftCommands(commands.Cog):
@@ -36,6 +41,78 @@ class MinecraftCommands(commands.Cog):
             raise Exception("Mojang's servers didn't return valid skin data.")
 
         return json.loads(base64.b64decode(val))
+
+    def _unpack_varint(self, sock):
+        data = 0
+        for i in range(5):
+            ordinal = sock.recv(1)
+
+            if len(ordinal) == 0:
+                break
+
+            byte = ord(ordinal)
+            data |= (byte & 0x7F) << 7*i
+
+            if not byte & 0x80:
+                break
+
+        return data
+
+    def _pack_varint(self, data):
+        ordinal = b''
+
+        while True:
+            byte = data & 0x7F
+            data >>= 7
+            ordinal += struct.pack('B', byte | (0x80 if data > 0 else 0))
+
+            if data == 0:
+                break
+
+        return ordinal
+
+    def _pack_data(self, data):
+        """ Page the data """
+        if isinstance(data, str):
+            data = data.encode('utf8')
+            return self._pack_varint(len(data)) + data
+        elif isinstance(data, int):
+            return struct.pack('H', data)
+        elif isinstance(data, float):
+            return struct.pack('L', int(data))
+        else:
+            return data
+
+    def _send_data(self, connection, *args):
+        """ Send the data on the connection """
+        data = b''
+
+        for arg in args:
+            data += self._pack_data(arg)
+
+        connection.send(self._pack_varint(len(data)) + data)
+
+    def _read_fully(self, connection, extra_varint=False):
+        """ Read the connection and return the bytes """
+        packet_length = self._unpack_varint(connection)
+        packet_id = self._unpack_varint(connection)
+        byte = b''
+
+        if extra_varint:
+            # Packet contained netty header offset for this
+            if packet_id > packet_length:
+                self._unpack_varint(connection)
+
+            extra_length = self._unpack_varint(connection)
+
+            while len(byte) < extra_length:
+                byte += connection.recv(extra_length)
+
+        else:
+            byte = connection.recv(packet_length)
+
+        return byte
+
 
     @commands.slash_command(guild_ids=[702953546106273852])
     async def minecraft(self, inter: disnake.ApplicationCommandInteraction):
@@ -95,6 +172,36 @@ class MinecraftCommands(commands.Cog):
 
         await inter.send(embeds=embeds)
 
+    def query_server(self, ip: str, port: str) -> Tuple[dict, int]:
+        valid = False
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+                conn.settimeout(5)
+                conn.connect((ip, port))
+                valid = True
+
+                # Send handshake + status request
+                self._send_data(conn, b'\x00\x00', ip, port, b'\x01')
+                self._send_data(conn, b'\x00')
+
+                # Read response, offset for string length
+                data = self._read_fully(conn, extra_varint=True)
+
+                # Send and read unix time
+                unix = time.time()
+                self._send_data(conn, b'\x01', 123)
+                _ = self._read_fully(conn)
+                ping = int((time.time() - unix) * 1000)
+
+                data = json.loads(data.decode('utf8'))
+                data['ping'] = ping
+
+                return data
+        except socket.timeout as e:
+            if not valid:
+                raise Exception('Could not connect to server. Make sure the IP is valid.')
+            raise e
+
     # TODO: Finish this
     @minecraft.sub_command()
     async def server(self,
@@ -112,9 +219,50 @@ class MinecraftCommands(commands.Cog):
         port = int(25565 if not ':' in address else address.split(':')[-1])
         ip = address.split(':')[0]
 
+        data = await self.bot.loop.run_in_executor(None, self.query_server, ip, port)
+
         embed = disnake.Embed(color=lynn.EMBED_COLOR)
+        embed.timestamp = datetime.utcnow()
         embed.title = ip + (f':{port}' if port != 25565 else '')
-        embed.description = 'WIP'
+        embed.set_footer(icon_url='https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/favicon-96x96.png')
+
+        if 'favicon' in data and data['favicon']:
+            favicon = disnake.File(BytesIO(base64.b64decode(data['favicon'].replace('data:image/png;base64,', ''))), 'pack.png')
+            embed.set_thumbnail(file=favicon)
+        else:
+            embed.set_thumbnail(url='https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/78/Pack_64x64.png')
+
+        if isinstance(data['description'], dict):
+            # json text format
+            desc = data['description']
+            description = ''
+            for obj in desc:
+                if obj == 'extra':
+                    for extra in desc[obj]:
+                        if 'text' in extra:
+                            description += extra['text']
+                elif obj == 'text':
+                    description += desc[obj]
+        else:
+            description = ''
+            for i in range(len(data['description'])):
+                # (Try to) clean up colorcodes from description
+                if (data['description'][i] == '§') or (i != 0 and data['description'][i-1] == '§'):
+                    continue
+                description += data['description'][i]
+        embed.description = description
+
+        embed.add_field('Version', data['version']['name'])
+        embed.add_field('Players', f"{data['players']['online']}/{data['players']['max']}")
+        embed.add_field('Ping', f"{data['ping']}ms")
+
+        if 'modinfo' in data:
+            if data['modinfo']['type'] == 'FML':
+                embed.add_field(name='Forge Server', value=f"{len(data['modinfo']['modList'])} mods enabled.")
+            else:
+                embed.add_field(name='Modded Server', value=f"Type: {data['modinfo']['type']}")
+        elif 'forgeData' in data:
+            embed.add_field(name='Forge Server', value=f"{len(data['forgeData']['mods'])} mods enabled.")
 
         await inter.send(embed=embed)
 
