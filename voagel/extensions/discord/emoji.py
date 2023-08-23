5fc27872a271e5984430c26082e873512d90d381
Fam0r <fam0r@mailbox.org>
2023-08-23 19:36:36 +0300

    Fix max emojis

--- voagel/extensions/discord/emoji.py
+++ voagel/extensions/discord/emoji.py
@@ -80,10 +80,10 @@ class EmojiCommand(commands.Cog):
         current_emojis = await inter.guild.fetch_emojis()
         current_static = [e for e in current_emojis if e.animated]
         current_animated = [e for e in current_emojis if not e.animated]
-        if len(static) > 100 or len(animated) > 100 or len(static) + len(current_static) > 100 or len(animated) + len(current_animated) > 100:
-            await inter.send(f'Found {len(static)} static and {len(animated)} animated emotes in zip.\nServer cannot fit more than {100-len(current_static)} static and {100-len(current_animated)} animated emotes.')
+        if len(static) > inter.guild.emoji_limit or len(animated) > inter.guild.emoji_limit or len(static) + len(current_static) > inter.guild.emoji_limit or len(animated) + len(current_animated) > inter.guild.emoji_limit:
+            await inter.send(f'Found {len(static)} static and {len(animated)} animated emotes in zip.\nServer cannot fit more than {inter.guild.emoji_limit-len(current_static)} static and {inter.guild.emoji_limit-len(current_animated)} animated emotes.')
             return
-
+            
         for emoji in static:
             await inter.guild.create_custom_emoji(name=emoji[0], image=emoji[1], reason=f'Mass-uploaded by {inter.author.name}')
         for emoji in animated:
