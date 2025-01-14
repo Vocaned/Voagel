fb8ad4aa1d409eb4ada820fab3ec0498d546f58d
Vocane <10358674+Vocaned@users.noreply.github.com>
2025-01-14 23:47:10 +0200

    Add whisper translation command

--- voagel/extensions/apis/whisper.py
+++ voagel/extensions/apis/whisper.py
@@ -17,18 +17,45 @@ class WhisperCommand(commands.Cog):
             name='Transcribe',
             callback=self.auto_transcribe
         ))
+        self.bot.tree.add_command(app_commands.ContextMenu(
+            name='Transcribe (Translate)',
+            callback=self.auto_translate
+        ))
 
-    async def do_transcribe(self, audio: bytes, content_type: str) -> str:
+
+    async def do_transcribe(self, audio: bytes, content_type: str, task: str = 'transcribe') -> str:
         data = aiohttp.FormData()
         data.add_field('audio_file',
                        audio,
                        content_type=content_type)
-        req = await self.bot.session.post(f'{self.bot.config["apis"]["whisper"]}/asr?encode=true&task=transcribe&vad_filter=true&output=txt', data=data)
+        req = await self.bot.session.post(f'{self.bot.config["apis"]["whisper"]}/asr?encode=true&task={task}&vad_filter=true&output=txt', data=data)
 
         if req.status != 200:
             raise Exception(f'Failed to transcribe file: {req.status}')
 
         return await req.text(encoding='utf-8')
+
+    async def auto_translate(self, inter: discord.Interaction, message: discord.Message):
+        """
+        Transcribe
+        """
+        if len(message.attachments) > 0:
+            link = message.attachments[0].url
+        else:
+            link = re.search(r'https?://\S+', message.content)
+            if not link:
+                raise Exception('Could not find audio in the specified message.')
+            link = link.group()
+
+        await inter.response.defer()
+        req = await self.bot.session.get(link)
+        audio = await req.read()
+        res = await self.do_transcribe(audio, req.content_type, task='translate')
+
+        embed = discord.Embed(color=EMBED_COLOR)
+        embed.set_footer(text='Whisper', icon_url=self.bot.get_asset('oai.png'))
+        embed.description = f'```\n{res}\n```'
+        await inter.followup.send(embed=embed)
 
     async def auto_transcribe(self, inter: discord.Interaction, message: discord.Message):
         """
