c92034c39fc1e91dcffafa8dbfe994dcc435ff2b
Vocaned <10358674+Vocaned@users.noreply.github.com>
2025-08-11 19:20:29 +0300

    fucking dumb ass library

--- voagel/extensions/core/admin.py
+++ voagel/extensions/core/admin.py
@@ -1,15 +1,18 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
-from voagel.main import Bot, ERROR_COLOR
-from voagel.utils import check_output
-from contextlib import redirect_stdout
 import textwrap
+from contextlib import redirect_stdout
 from io import StringIO
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+from voagel.main import ERROR_COLOR, Bot
+from voagel.utils import check_output
+
 
 async def is_admin(inter: discord.Interaction) -> bool:
     assert isinstance(inter.client, commands.Bot)
     return await inter.client.is_owner(inter.user)
+
 
 class AdminCommands(commands.Cog):
     """Admin commands"""
@@ -17,9 +20,11 @@ class AdminCommands(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    admin = app_commands.Group(name='admin', description='Administration commands')
-    module = app_commands.Group(name='module', description='Module commands', parent=admin)
-    git = app_commands.Group(name='git', description='Git commands', parent=admin)
+    admin = app_commands.Group(name="admin", description="Administration commands")
+    module = app_commands.Group(
+        name="module", description="Module commands", parent=admin
+    )
+    git = app_commands.Group(name="git", description="Git commands", parent=admin)
 
     admin.interaction_check = is_admin
     module.interaction_check = is_admin
@@ -30,61 +35,62 @@ class AdminCommands(commands.Cog):
         """Syncs all slash commands"""
         await inter.response.defer(thinking=True)
         await self.bot.tree.sync()
-        await inter.followup.send('Synced commands.')
+        await inter.followup.send("Synced commands.")
 
-    class EvalModal(discord.ui.Modal, title='Eval'):
+    class EvalModal(discord.ui.Modal, title="Eval"):
         code = discord.ui.TextInput(
-            label='Python Code',
-            custom_id='code',
+            label="Python Code",
+            custom_id="code",
             style=discord.TextStyle.paragraph,
-            required=True
+            required=True,
         )
 
         async def on_submit(self, inter: discord.Interaction) -> None:
             code = self.code.value
             """Eval"""
             env = {
-                'bot': inter.client,
-                'inter': inter,
-                'channel': inter.channel,
-                'user': inter.user,
-                'guild': inter.guild
+                "bot": inter.client,
+                "inter": inter,
+                "channel": inter.channel,
+                "user": inter.user,
+                "guild": inter.guild,
             }
             env.update(globals())
 
-            if code.startswith('```') and code.endswith('```'):
-                code = '\n'.join(code.split('\n')[1:-1])
+            if code.startswith("```") and code.endswith("```"):
+                code = "\n".join(code.split("\n")[1:-1])
             else:
-                code = code.strip('` \n')
+                code = code.strip("` \n")
 
             stdout = StringIO()
 
             to_compile = f'async def func():\n{textwrap.indent(code, "  ")}'
             exec(to_compile, env)
 
-            func = env['func']
+            func = env["func"]
             with redirect_stdout(stdout):
                 try:
                     ret = await func()
                 except Exception as e:
-                    embed = discord.Embed(color=ERROR_COLOR, title=type(e).__name__, description=e)
+                    embed = discord.Embed(
+                        color=ERROR_COLOR, title=type(e).__name__, description=e
+                    )
                     await inter.response.send_message(embed=embed)
                     return
                 value = stdout.getvalue()
 
             if inter.response.is_done():
-                return # Message sent from eval, don't make an another one
+                return  # Message sent from eval, don't make an another one
             else:
-                output = ''
+                output = ""
                 if ret:
-                    output += f'Return: ```\n{ret}\n```\n'
+                    output += f"Return: ```\n{ret}\n```\n"
                 if value:
-                    output += f'```\n{value}\n```\n'
+                    output += f"```\n{value}\n```\n"
                 if not output:
-                    output = '[No response]'
+                    output = "[No response]"
                 await inter.response.send_message(output)
 
-    @commands.is_owner()
     @admin.command()
     async def eval(self, inter: discord.Interaction):
         await inter.response.send_modal(self.EvalModal())
@@ -98,8 +104,8 @@ class AdminCommands(commands.Cog):
         module: Name of the module
         """
         await inter.response.defer(thinking=True)
-        await self.bot.load_extension('voagel.extensions.'+module)
-        await inter.followup.send(f'Loaded {module}')
+        await self.bot.load_extension("voagel.extensions." + module)
+        await inter.followup.send(f"Loaded {module}")
 
     @module.command()
     async def unload(self, inter: discord.Interaction, module: str):
@@ -110,8 +116,8 @@ class AdminCommands(commands.Cog):
         module: Name of the module
         """
         await inter.response.defer(thinking=True)
-        await self.bot.unload_extension('voagel.extensions.'+module)
-        await inter.followup.send(f'Unloaded {module}')
+        await self.bot.unload_extension("voagel.extensions." + module)
+        await inter.followup.send(f"Unloaded {module}")
 
     @module.command()
     async def reload(self, inter: discord.Interaction, module: str):
@@ -122,21 +128,26 @@ class AdminCommands(commands.Cog):
         module: Name of the module
         """
         await inter.response.defer(thinking=True)
-        await self.bot.reload_extension('voagel.extensions.'+module)
-        await inter.followup.send(f'Reloaded {module}')
+        await self.bot.reload_extension("voagel.extensions." + module)
+        await inter.followup.send(f"Reloaded {module}")
 
     @git.command()
     async def pull(self, inter: discord.Interaction):
         """Pull the git repo"""
         await inter.response.defer(thinking=True)
-        out1 = await check_output(['git', 'pull'], timeout=30)
-        out2 = await check_output(['git', 'log', '@{1}..', '--format=%h %an | %B'], timeout=30)
-        await inter.followup.send(f'```{out1}\n\n{out2}```')
+        out1 = await check_output(["git", "pull"], timeout=30)
+        out2 = await check_output(
+            ["git", "log", "@{1}..", "--format=%h %an | %B"], timeout=30
+        )
+        await inter.followup.send(f"```{out1}\n\n{out2}```")
 
     @git.command()
     async def fuck(self, inter: discord.Interaction):
         """Did you fuck up the files again?"""
-        await inter.response.send_message(f'```{await check_output(["git", "reset", "--hard", "origin/master"])}```')
+        await inter.response.send_message(
+            f'```{await check_output(["git", "reset", "--hard", "origin/master"])}```'
+        )
+
 
 async def setup(bot: Bot):
     await bot.add_cog(AdminCommands(bot))
--- voagel/main.py
+++ voagel/main.py
@@ -1,14 +1,16 @@
 import logging
-import discord
 import sys
 import tomllib
-import aiohttp
 from pathlib import Path
-from discord.ext import commands
-from discord import app_commands
 
-ERROR_COLOR = 0xff4444
-EMBED_COLOR = 0x8f8f8f
+import aiohttp
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+ERROR_COLOR = 0xFF4444
+EMBED_COLOR = 0x8F8F8F
+
 
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
@@ -18,50 +20,79 @@ class Bot(commands.Bot):
         super().__init__(*args, **kwargs)
 
     async def on_ready(self) -> None:
-        self.session = aiohttp.ClientSession(headers={'User-Agent': 'Voagel (Discord Bot) https://github.com/Vocaned/Voagel'})
+        self.session = aiohttp.ClientSession(
+            headers={
+                "User-Agent": "Voagel (Discord Bot) https://github.com/Vocaned/Voagel"
+            }
+        )
         logging.info("Bot is ready.")
 
     async def setup_hook(self) -> None:
         # TODO: clean up this mess
-        for extension in [str(f.resolve()).replace('.py', '').replace('/', '.').replace('\\', '.').split('voagel.extensions.')[-1] for f in Path(__file__).parent.rglob('extensions/**/*.py')]:
+        for extension in [
+            str(f.resolve())
+            .replace(".py", "")
+            .replace("/", ".")
+            .replace("\\", ".")
+            .split("voagel.extensions.")[-1]
+            for f in Path(__file__).parent.rglob("extensions/**/*.py")
+        ]:
             try:
-                await self.load_extension('voagel.extensions.' + extension)
+                await self.load_extension("voagel.extensions." + extension)
             except (discord.ClientException, ModuleNotFoundError):
-                logging.error('Failed to load extension %s', extension, exc_info=True)
+                logging.error("Failed to load extension %s", extension, exc_info=True)
 
     def get_api_key(self, key: str) -> str:
-        val = self.config['secrets'].get(key, None)
+        val = self.config["secrets"].get(key, None)
         if not val:
-            raise commands.DisabledCommand('API key for command missing. Ping the bot owner for help.')
+            raise commands.DisabledCommand(
+                "API key for command missing. Ping the bot owner for help."
+            )
         return val
 
     def get_asset(self, path: str) -> str:
-        return self.config['misc']['asset_url'] + path
+        return self.config["misc"]["asset_url"] + path
 
     def load_config(self) -> None:
-        Path('config').mkdir(exist_ok=True)
-        if not Path('config', 'bot.toml').exists():
-            logging.critical('Bot config could not be found. Copy bot.toml to the config folder and edit it to continue.')
+        Path("config").mkdir(exist_ok=True)
+        if not Path("config", "bot.toml").exists():
+            logging.critical(
+                "Bot config could not be found. Copy bot.toml to the config folder and edit it to continue."
+            )
             sys.exit(1)
-        with open(Path('config', 'bot.toml'), 'rb') as f:
+        with open(Path("config", "bot.toml"), "rb") as f:
             self.config = tomllib.load(f)
+
 
 def main() -> None:
     bot = Bot(
         intents=discord.Intents.all(),
         allowed_installs=app_commands.AppInstallationType(guild=True, user=True),
-        allowed_contexts=app_commands.AppCommandContext(guild=True, dm_channel=True, private_channel=True),
-        command_prefix='whythefuckdoihavetoprovideacommandprefixidonthaveanytextcommands...',
+        allowed_contexts=app_commands.AppCommandContext(
+            guild=True, dm_channel=True, private_channel=True
+        ),
+        command_prefix="whythefuckdoihavetoprovideacommandprefixidonthaveanytextcommands...",
     )
     bot.load_config()
 
-    bot.status = discord.Status(bot.config['bot']['status'])
+    bot.status = discord.Status(bot.config["bot"]["status"])
 
-    activities = {'unknown': -1, 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5}
-    activitytype = activities.get(bot.config['bot']['activity_type'].lower(), 0)
-    bot.activity = discord.Activity(name=bot.config['bot']['activity'], type=activitytype)
+    activities = {
+        "unknown": -1,
+        "playing": 0,
+        "streaming": 1,
+        "listening": 2,
+        "watching": 3,
+        "custom": 4,
+        "competing": 5,
+    }
+    activitytype = activities.get(bot.config["bot"]["activity_type"].lower(), 0)
+    bot.activity = discord.Activity(
+        name=bot.config["bot"]["activity"], type=activitytype
+    )
 
-    bot.run(bot.get_api_key('discord'))
+    bot.run(bot.get_api_key("discord"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
